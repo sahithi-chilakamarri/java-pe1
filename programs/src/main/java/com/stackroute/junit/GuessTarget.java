8d462bf63d5202db71dbaf8bbae9f5625e0e7ff3
sahithi-chilakamarri <51943021+sahithi-chilakamarri@users.noreply.github.com>
2019-07-08 12:31:27 +0530

    Update GuessTarget.java

--- programs/src/main/java/com/stackroute/junit/GuessTarget.java
+++ programs/src/main/java/com/stackroute/junit/GuessTarget.java
@@ -2,7 +2,7 @@ package com.stackroute.junit;
 //This is used to check whether the guess is equal to target.
 public class GuessTarget {
 
-    public static String guessTarget(int guess, int target){
+    public String guessTarget(int guess, int target){
         String string="";
         //When number is greater than the guess.
         if(target>guess)
